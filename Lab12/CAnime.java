4de505ee0075af9ce943b855f5f5c1b54e7bd431
22p21n0056-chan <60224796+channo39mz@users.noreply.github.com>
2023-08-07 00:14:49 +0700

    Add files via upload

---
+++ Lab12/CAnime.java
@@ -0,0 +1,11 @@
+package Lab12;
+
+public class CAnime {
+    private int anime_id;
+    private String name;
+    private String genre;
+    private String type;
+    private int ep;
+    private float rating;
+    private int member;
+}
